672d038379724297d02053cab6334931d28af2d6
Giulio Zelante <giulio.zelante@gmail.com>
2017-11-10 19:19:04 +0100

    added gulp util

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 'use strict';
 
-var uglifyPHP = require("uglify-php"), path = require("path");
-module.exports = function(a, b) {
-    if (!a) {
-        throw Error("gulp-php-obfuscator: Missing file option");
-    }
-    b = b || {};
-    if ("string" !== typeof a) {
-        if ("string" === typeof a.path) {
-            path.basename(a.path);
-        } else {
-            throw Error("gulp-php-obfuscator: Missing path in file options");
+const uglifyPHP = require('uglify-php');
+
+var through = require('through2');
+var gutil = require('gulp-util')
+
+// file can be a vinyl file object or a string
+// when a string it will construct a new one
+module.exports = function(opt) {
+    return through.obj(function (file, enc, callback) {
+        var isBuffer = false,
+            inputString = null,
+            result = null,
+            outBuffer=null;
+        //Empty file and directory not supported
+        if (file === null || file.isDirectory()) {
+            this.push(file);
+            return callback();
         }
-    }
-    uglifyPHP.minify(a, b).then(function(a) {
-        return a;
+        isBuffer = file.isBuffer();
+        if(isBuffer){
+            uglifyPHP.minify(file.path, opt).then(function (result) {
+                outBuffer = new Buffer(result);
+                var aFile = new gutil.File();
+                aFile.path = file.path;
+                aFile.contents = outBuffer;
+                callback(null,aFile);
+            });
+        }else{
+            this.emit('error',
+                new PluginError("gulp-php-obfuscator",
+                    'Only Buffer format is supported'));
+            callback();
+        }
     });
 };
